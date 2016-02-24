5f65cd2154950caa035a774cc2e60ec7653a0703
pgrassal <thebigone@grassal-mueller.de>
2016-02-24 18:07:05 +0100

    Changed test output

--- test/PeakTestsMain.cpp
+++ test/PeakTestsMain.cpp
@@ -11,7 +11,7 @@
 #include "gtest/gtest.h"
 
 GTEST_API_ int main(int argc, char **argv) {
-  printf("Running main() from gtest_main.cc\n");
+  printf("Run peak tests...\n");
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
